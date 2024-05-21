f7f1ea85ed3441f68d559ead020108ce63c86f87
Turing0x <83433135+Turing0x@users.noreply.github.com>
2024-05-21 17:02:06 -0400

    inicio loas trabajos con los sales y cambios en productos

--- src/api/Products/infraestructure/product.controllers.ts
+++ src/api/Products/infraestructure/product.controllers.ts
@@ -1,93 +1,105 @@
-// import { Response, Request } from 'express';
+import { Response, Request } from 'express';
 
-// import { sendRes } from '../../../helpers/send.res';
-// import { Product } from '../interface/product.interface';
-// import { ProductModel } from '../models/product.model';
+import { sendRes } from '../../../helpers/send.res';
+import { ProductsManager } from './product-querys';
+import { Product } from '../interface/product.interface';
+export class ProductsControllers {
 
-// export class ProductsControllers {
+  static async getAllProducts (req: Request, res: Response) {
 
-//   static async getAllProducts (req: Request, res: Response) {
+    try {
+      const products = await ProductsManager('SelectAll')
+      return sendRes(res, 200, true, 'Datos Obtenidos', products);
+    } catch (error) { 
+      if (error instanceof Error) {
+        return sendRes(res, 500, false, 'Error Grave', error.message); 
+      } else {
+        return sendRes(res, 500, false, 'Error Grave', '');
+      }
+    }
 
-//     try {
-//       const products = await ProductModel.find()
-//         .populate(['measure_unit', 'clasification', 'concept'])
-//         .lean();;
-//       return sendRes(res, 200, true, 'Datos Obtenidos', products)
-//       ;
-//     } catch (error) { 
-//       if (error instanceof Error) {
-//         return sendRes(res, 500, false, 'Error Grave', error.message); 
-//       } else {
-//         return sendRes(res, 500, false, 'Error Grave', '');
-//       }
-//     }
+  }
 
-//   }
+  static async getProductsById (req: Request, res: Response) {
 
-//   static async getProductsById (req: Request, res: Response) {
+    try {
 
-//     try {
-
-//       const { clientId } = req.params;
-//       if (!clientId) return sendRes(res,
-//         500,
-//         false,
-//         'Error Grave', ''); 
+      const { id } = req.params;
+      if (!id) return sendRes(res,
+        200,
+        false,
+        'Faltan datos para realizar esta acción', ''); 
     
-//       const user = await ProductModel.findById(clientId)
-//         .populate(['measure_unit', 'clasification', 'concept'])
-//         .lean();
-//       if (!user) return sendRes(res, 500, false, 'Usuario no encontrado', ''); 
+      const user = await ProductsManager('SelectById', {Id: id});
+      if (!user) return sendRes(res, 200, false, 'Usuario no encontrado', ''); 
       
-//       return sendRes(res, 500, false, 'Resultado de la búsqueda', user); 
+      return sendRes(res, 200, true, 'Resultado de la búsqueda', user); 
       
-//     } catch (error) { 
-//       if (error instanceof Error) {
-//         return sendRes(res, 500, false, 'mess_0', error.message); 
-//       } else {
-//         return sendRes(res, 500, false, 'mess_0', '');
-//       }
-//     }
+    } catch (error) { 
+      if (error instanceof Error) {
+        return sendRes(res, 500, false, 'mess_0', error.message); 
+      } else {
+        return sendRes(res, 500, false, 'mess_0', '');
+      }
+    }
 
-//   }
+  }
 
-//   static async saveProduct(req: Request, res: Response) {
+  static async saveProduct(req: Request, res: Response) {
   
-//     try {
+    try {
 
-//       const data: Product = req.body;
+      const data: Product = req.body;
+      const fetchedProduct: Product = await (await ProductsManager('SelectByName', { Nombre: data.Nombre })).at(0)
+
+      if (fetchedProduct) {
+        await ProductsManager('Update', data);
+        return sendRes(res, 200, true, 'Usuario Editado Correctamente', '');
+      }
+
+      await ProductsManager('Insert', data);
+      return sendRes(res, 200, true, 'Usuario Creado Exitosamente', '');
       
-//         const prod = new ProductModel(data);
-//         await prod.save();
-        
+    } catch (error) {
+      return sendRes(res, 500, false, 'Ha ocurrido algo grave', error);
+    }
+
+  }
+
+  static async deleteProduct (req: Request, res: Response) {
+
+    try {
       
-
-//       return sendRes(res, 200, true, 'Usuario Creado Exitosamente', '');
+      const { id } = req.params;
+      if (!id) return sendRes(res, 200, false, 'Faltan datos para realizar esta acción', ''); 
       
-//     } catch (error) {
-//       return sendRes(res, 500, false, 'Ha ocurrido algo grave', error);
-//     }
-
-//   }
-
-//   static async deleteProduct (req: Request, res: Response) {
-
-//     try {
-      
-//       const { id } = req.params;
-//       if( !id ) return sendRes(res, 500, false, 'Usuario no encontrado', ''); 
+      console.log(id);
     
-//       await ProductModel.deleteOne({ _id: id })
-//       return sendRes(res, 200, true, 'Usuario Eliminado Correctamente', '');
+      await ProductsManager('Delete', {Id: id});
+      return sendRes(res, 200, true, 'Usuario Eliminado Correctamente', '');
 
-//     } catch (error) { 
-//       if (error instanceof Error) {
-//         return sendRes(res, 500, false, 'Error Interno', error.message); 
-//       } else {
-//         return sendRes(res, 500, false, 'Error Interno', '');
-//       }
-//     }
+    } catch (error) { 
+      if (error instanceof Error) {
+        console.log(error);
+        return sendRes(res, 500, false, 'Error Interno', error.message); 
+      } else {
+        return sendRes(res, 500, false, 'Error Interno', '');
+      }
+    }
 
-//   }
+  }
 
-// }
+  static async changeActive(req: Request, res: Response) {
+    try {
+
+      const data: Product = req.body
+
+      await ProductsManager('Erease', data);
+      return sendRes(res, 200, true, 'Usuario Editado', '');
+
+    } catch (error) {
+      return sendRes(res, 500, false, 'Error Interno', '');
+    }
+  }
+
+}
---
+++ src/api/Sales/router/sales.router.ts
@@ -0,0 +1,17 @@
+import { Router } from "express"
+
+import { SalesControllers } from '../infraestructure/sales.controllers';
+import { checkAuth } from "../../../helpers/checkAuth";
+
+const router = Router()
+
+router
+
+  .get('/', checkAuth, SalesControllers.getAllSales)
+  .get('/:id', checkAuth, SalesControllers.getSalesById)
+
+  .post('/', checkAuth, SalesControllers.saveSale)
+
+  .delete('/:id', checkAuth, SalesControllers.deleteSale)
+
+export const SalesRouter = router
